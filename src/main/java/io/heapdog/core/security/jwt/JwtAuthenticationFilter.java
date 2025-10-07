29a5aee48eda3a7feec4abd6f14cf5fa52e10cc9
parthokr <partho.kr@proton.me>
2025-10-07 11:22:09 +0000

    fix(import): remove unused import

--- src/main/java/io/heapdog/core/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/io/heapdog/core/security/jwt/JwtAuthenticationFilter.java
@@ -7,9 +7,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
-
 import org.springframework.lang.NonNull;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
