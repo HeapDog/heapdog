ffbfac7df512454678b3b54cda6058b52f6f52a9
R-emon <rabiulislamemon1@gmail.com>
2025-10-10 23:51:53 +0600

    feat(Model): Added model class for PasswordResetOtp

---
+++ src/main/java/io/heapdog/core/model/PasswordResetOtp.java
@@ -0,0 +1,32 @@
+package io.heapdog.core.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordResetOtp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String otp;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    @OneToOne(targetEntity = HeapDogUser.class, fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "user_id")
+    private HeapDogUser user;
+}
