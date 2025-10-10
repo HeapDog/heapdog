ea2ffbfed7093519ef62c7eca0cd27486ae1a902
R-emon <rabiulislamemon1@gmail.com>
2025-10-10 23:50:18 +0600

    feat(DTO): Added PasswordResetResponseDto

---
+++ src/main/java/io/heapdog/core/dto/PasswordResetResponseDto.java
@@ -0,0 +1,14 @@
+package io.heapdog.core.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PasswordResetResponseDto {
+    private String message;
+}
