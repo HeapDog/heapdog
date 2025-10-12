ec97290c3ac063dbdc82d477c6560204dbcd084c
parthokr <partho.kr@proton.me>
2025-10-13 02:26:55 +0600

    feat(builder): use builder pattern

--- src/main/java/io/heapdog/core/dto/PasswordResetRequestDto.java
+++ src/main/java/io/heapdog/core/dto/PasswordResetRequestDto.java
@@ -2,9 +2,11 @@ package io.heapdog.core.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class PasswordResetRequestDto {
 
     @NotBlank(message = "Email is required")
